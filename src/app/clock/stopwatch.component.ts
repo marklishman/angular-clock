eae856bfbfa921d7f435ab666166ed03847c49e9
marklishman <mark.lishman@gmail.com>
2018-01-06 14:29:16 +0000

    More concise stopwatch

--- src/app/clock/stopwatch.component.ts
+++ src/app/clock/stopwatch.component.ts
@@ -3,31 +3,23 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-stopwatch',
   template: `
-    <p *ngIf="elapsedSeconds">
+    <p *ngIf="elapsedTime">
       {{elapsedTime | date:'HH:mm:ss'}} <span class="half-size">{{decisecond}}</span>
     </p>
   `,
   styles: ['.half-size {font-size: 50%;}']
 })
 export class StopwatchComponent implements OnInit {
+  elapsedTime: Date;
   decisecond = 0;
-  startTimeSeconds: number = Math.floor(new Date().getTime() / 1000);
-  elapsedSeconds: number;
+
+  private startTimeSeconds = new Date().getTime();
 
   ngOnInit(): void {
-    this.calculateDeciseconds();
-  }
-
-  calculateDeciseconds(): void {
     setInterval(() => {
-      const millis = new Date().getTime();
-      this.elapsedSeconds = Math.floor(millis / 1000) - this.startTimeSeconds;
-      this.decisecond = Math.floor(millis / 100 % 10);
+      const now = new Date().getTime();
+      this.elapsedTime = new Date(now - this.startTimeSeconds);
+      this.decisecond = Math.floor((now / 100) % 10);
     }, 100);
   }
-
-  get elapsedTime(): Date {
-    return new Date(this.elapsedSeconds * 1000);
-  }
-
 }
